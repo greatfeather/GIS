943926950a0ff4506682dd8cbcc6a94ecbe552be
greatfeather <peifei@126.com>
2017-02-16 13:39:32 +0800

    测试

---
+++ Extend_SecurityInfo/src/com/supermap/services/security/test.java
@@ -0,0 +1,13 @@
+package com.supermap.services.security;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello test!");
+	}
+
+}
